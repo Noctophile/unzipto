27276c6459eb167c20471404f67a8c37dff2b138
Rizz <123063244+Noctophile@users.noreply.github.com>
2024-12-08 03:49:02 -0800

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ import os
 
 class Config(object):
     # Mandotory
-    APP_ID = int(os.environ.get("21857983"))
-    API_HASH = os.environ.get("e469e84c943ce3b8b056eb6a296f2c67")
-    BOT_TOKEN = os.environ.get("7994710683:AAEc8wbxnNI06rIh611TyL_1CXcLmR4igTY")
-    LOGS_CHANNEL = int(os.environ.get("-1002164681451"))
-    BOT_OWNER = int(os.environ.get("833465134"))
-    MONGODB_URL = os.environ.get("mongodb+srv://dhimanrajat:Y8IAGI0lVrMhjvkU@cluster0.mytkgu6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    APP_ID = int(os.environ.get("APP_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    MONGODB_URL = os.environ.get("MONGODB_URL")
     GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN")
     # Optional
     MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
